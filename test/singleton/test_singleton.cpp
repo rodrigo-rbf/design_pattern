e28b4eebe1c2886a0bfbf3a3413a465a23758de3
Rodrigo de Britto Florencio <rodrigobf@gmail.com>
2024-03-25 21:13:01 -0300

    upgrade test_singleton.cpp

--- test/singleton/test_singleton.cpp
+++ test/singleton/test_singleton.cpp
@@ -1,31 +1,66 @@
 #include <vector>
+#include <csignal>
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <time.h>
+#include<unistd.h>
+
 
 #include "singleton/singleton.hpp"
 
+volatile std::sig_atomic_t done = 0;
+
+void loop_thread_1()
+{
+    while(!done){
+        auto& sgl = Singleton<double>::create_instance();
+        if (sgl.get_msg() == "loop_thread_2")
+        {
+            sgl.set_msg("loop_thread_1");
+        }
+       sleep(4);
+    }
+}
+
+void loop_thread_2()
+{
+    while(!done){
+        auto& sgl = Singleton<double>::create_instance();
+        if (sgl.get_msg() == "loop_thread_1")
+        {
+            sgl.set_msg("loop_thread_2");
+        }
+       sleep(4);
+    }
+}
+
 int main()
 {
+    
+    std::thread loop_1(loop_thread_1);
+    sleep(2);
+    std::thread loop_2(loop_thread_2);
 
-    std::vector<std::thread> threads;
+    auto& sgl_main = Singleton<double>::create_instance();
+    sgl_main.set_msg("loop_thread_2");
 
-    for(int i = 0; i< 10; ++i)
+    for(int i = 0; i< 5; ++i)
     {
-        threads.emplace_back(std::thread([](){
-            auto& sgl = Singleton<double>::create_instance();
-            sgl.set_msg("correct ->");
-        }));
-
-        for (auto& thr : threads)
-        {
-            if(thr.joinable())
-            {
-                thr.join();
-            }
-        }
+        std::cout << i << ": " << sgl_main.get_msg() << '\n';
+        sleep(2);
     }
 
-    auto& sgl_02 = Singleton<int>::create_instance();
-    std::cout << sgl_02.get_msg() << '\n';
-     
+    sleep(2);
+    done = true;
+
+    if (loop_1.joinable()) {
+        loop_1.join(); 
+    }
+
+    if (loop_2.joinable()) {
+        loop_2.join();
+    }
 
     return 0;
 }
